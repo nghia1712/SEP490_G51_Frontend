8a92aa74bd4ca372a09f01bdbb6699ce49f4df34
tuna <tuna@sts.vn>
2025-11-22 12:24:55 +0700

    add restore

--- src/App.jsx
+++ src/App.jsx
@@ -711,7 +711,7 @@ function App() {
                         element={
                           <ProtectedRoute
                             allowedRoles={[
-                              "manager",
+                              "sales_staff",
                               "warehouse_staff",
                               "admin",
                             ]}
